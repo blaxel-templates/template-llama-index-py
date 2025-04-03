0e4a94e35eae722842b5e50512458728be2c0aaf
cploujoux <cploujoux@blaxel.ai>
2025-04-03 16:28:14 +0200

    Update model

--- src/agent.py
+++ src/agent.py
@@ -13,7 +13,7 @@ async def weather(city: str) -> str:
 async def agent(input: str) -> AsyncGenerator[str, None]:
     prompt = "You are a helpful assistant that can answer questions and help with tasks."
     tools = await bl_tools(["blaxel-search"]).to_llamaindex() + [FunctionTool.from_defaults(async_fn=weather)]
-    model = await bl_model("gpt-4o-mini").to_llamaindex()
+    model = await bl_model("sandbox-openai").to_llamaindex()
     agent = ReActAgent(llm=model, tools=tools, system_prompt=prompt)
     async for event in agent.run(input).stream_events():
         if isinstance(event, AgentStream):
