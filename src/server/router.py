385979e43509484e6a89e849da0fef6745568011
cploujoux <cploujoux@blaxel.ai>
2025-04-03 08:53:12 -0500

    Initial commit

---
+++ src/server/router.py
@@ -0,0 +1,19 @@
+from blaxel.instrumentation.span import SpanManager
+from fastapi import APIRouter
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+
+from agent import agent
+
+router = APIRouter()
+
+class RequestInput(BaseModel):
+    inputs: str
+
+@router.post("/")
+async def handle_request(request: RequestInput):
+    with SpanManager("blaxel-langchain-deepresearch").create_active_span("agent-request", {}):
+        return StreamingResponse(
+            agent(request.inputs),
+            media_type='text/event-stream'
+        )
